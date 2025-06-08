d2d873c7e7ae381fe0d2a084c9f26de8b4f85084
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-06-08 20:18:04 +0530

    Innitial commit

---
+++ lib/features/auth/data/auth_request.dart
@@ -0,0 +1,11 @@
+class AuthRequest {
+  final String name;
+  final String email;
+  final String password;
+
+  AuthRequest({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+}
