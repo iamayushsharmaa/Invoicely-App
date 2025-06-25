16adb2d0fa3420f41785e2f076560c04c4afe358
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-06-25 23:30:09 +0530

    built authentication screens

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:invoice/core/network/dio_client.dart';
+import 'package:invoice/features/auth/domain/repository/auth_repository_impl.dart';
 import 'package:invoice/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:invoice/features/auth/presentation/screens/onboarding_screen.dart';
 import 'package:invoice/features/auth/presentation/screens/signin_screen.dart';
 import 'package:invoice/features/auth/presentation/screens/signup_screen.dart';
 import 'package:invoice/features/home/presentation/widget_tree.dart';
 
+import '../features/auth/data/remote/auth_api_service.dart';
 import '../features/home/presentation/home_screen.dart';
 
 GoRouter createRouter(AuthBloc authBloc) {
-
   final initialLocation = authBloc.state is Authenticated
       ? '/home'
       : '/onboarding';
+
+  final dio = DioClient.create();
+  final authService = AuthService(
+    dio: dio,
+    googleSignIn: GoogleSignIn(),
+    storage: FlutterSecureStorage(),
+  );
+  final authRepository = AuthRepositoryImpl(authService);
 
   return GoRouter(
     debugLogDiagnostics: true,
@@ -23,7 +35,10 @@ GoRouter createRouter(AuthBloc authBloc) {
       GoRoute(
         path: '/onboarding',
         name: 'onBoarding',
-        builder: (context, state) => const OnBoardingScreen(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => AuthBloc(authRepository),
+          child: OnBoardingScreen(),
+        ),
       ),
       GoRoute(
         path: '/signin',
@@ -69,12 +84,11 @@ GoRouter createRouter(AuthBloc authBloc) {
   );
 }
 
-
 class GoRouterRefresh extends ChangeNotifier {
   final AuthBloc bloc;
 
   GoRouterRefresh(this.bloc) {
-    bloc.stream.listen(( _) {
+    bloc.stream.listen((_) {
       notifyListeners();
     });
   }
