a35fcf3a5bf483f41d9526e3b172e7039d4b3f7a
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-06-29 21:04:43 +0530

    client-feature

---
+++ lib/features/add_client/presentation/screens/add_client_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddClientScreen extends StatefulWidget {
+  const AddClientScreen({super.key});
+
+  @override
+  State<AddClientScreen> createState() => _AddClientScreenState();
+}
+
+class _AddClientScreenState extends State<AddClientScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
