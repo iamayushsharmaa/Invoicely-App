e0d42b03686534444367ed44a78d66047dd41727
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-07-15 23:30:37 +0530

    invoice screens

---
+++ lib/features/invoice/presentation/screens/add_invoice.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddInvoiceScreen extends StatefulWidget {
+  const AddInvoiceScreen({super.key});
+
+  @override
+  State<AddInvoiceScreen> createState() => _AddInvoiceScreenState();
+}
+
+class _AddInvoiceScreenState extends State<AddInvoiceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
