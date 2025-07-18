4937dda9d7df17849520a9a871c1733175caa350
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-07-18 20:09:31 +0530

    invoice screens

--- lib/features/client/presentation/screens/client_screen.dart
+++ lib/features/client/presentation/screens/client_screen.dart
@@ -10,9 +10,29 @@ class ClientScreen extends StatefulWidget {
 class _ClientScreenState extends State<ClientScreen> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController searchController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Clients', style: TextStyle(fontSize: 30, color: Colors.black),),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50,
+            width: double.infinity,
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: 'Search client...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/features/invoice/presentation/screens/invoices_screen.dart
+++ lib/features/invoice/presentation/screens/invoices_screen.dart
@@ -10,9 +10,32 @@ class InvoiceScreen extends StatefulWidget {
 class _InvoiceScreenState extends State<InvoiceScreen> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController searchController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Invoices', style: TextStyle(fontSize: 30, color: Colors.black),),
+        title: Text(
+          'Invoices',
+          style: TextStyle(fontSize: 30, color: Colors.black),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50,
+            width: double.infinity,
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: 'Search invoices...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
