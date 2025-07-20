a9eb2c94cc0c769c771f96592719ebff32e6d46e
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-07-20 21:33:06 +0530

    invoice api integration

--- lib/features/invoice/data/remote/InvoiceApiService.dart
+++ lib/features/invoice/data/remote/InvoiceApiService.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:invoice/features/invoice/data/model/invoice_model.dart';
 
 class InvoiceApiService {
   final Dio dio;
@@ -6,5 +7,60 @@ class InvoiceApiService {
 
   InvoiceApiService({required this.dio});
 
+  Future<InvoiceModel> createInvoice(InvoiceModel request) async {
+    try {
+      final response = await dio.post(
+        '/api/v1/invoice',
+        data: request.toMap(),
+      );
+      return InvoiceModel.fromMap(response.data);
+    } catch (e) {
+      print('Error: \$e');
+    }
+  }
 
+  Future<InvoiceModel> searchInvoice(InvoiceModel request) async {
+    try {
+      final response = await dio.get(
+        '/api/v1/invoice',
+        queryParameters: request.toMap(),
+      );
+      return InvoiceModel.fromMap(response.data);
+    } catch (e) {
+      print('Error: \$e');
+    }
+  }
+
+
+  Future<InvoiceModel> getAllInvoices(InvoiceModel request) async {
+    try {
+
+    } catch (e) {
+      print('Error: \$e');
+    }
+  }
+
+  Future<InvoiceModel> getInvoiceById(InvoiceModel request) async {
+    try {
+
+    } catch (e) {
+      print('Error: \$e');
+    }
+  }
+
+  Future<InvoiceModel> updateInvoice(InvoiceModel request) async {
+    try {
+
+    } catch (e) {
+      print('Error: \$e');
+    }
+  }
+
+  Future<InvoiceModel> deleteInvoice(InvoiceModel request) async {
+    try {
+
+    } catch (e) {
+      print('Error: \$e');
+    }
+  }
 }
