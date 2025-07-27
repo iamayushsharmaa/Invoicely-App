963fae6a4c8dab005f6acedaa9993bbbc495a353
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-07-27 22:20:58 +0530

    client api

--- lib/features/client/data/remote/client_api_service.dart
+++ lib/features/client/data/remote/client_api_service.dart
@@ -1,3 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:invoice/features/client/data/models/client_model.dart';
+
 class ClientApiService{
 
+
+  final Dio dio;
+  static final String baseUrl = 'https://invoicely-367c.onrender.com';
+
+  ClientApiService({required this.dio});
+
+  Future<ClientModel> createClient(ClientModel request) async {
+    try {
+      final response = await dio.post(
+        '/api/v1/invoice',
+        data: request.toMap(),
+      );
+      return ClientModel.fromMap(response.data);
+    } catch (e) {
+      print('Error: \$e');
+    }
+  }
 }
