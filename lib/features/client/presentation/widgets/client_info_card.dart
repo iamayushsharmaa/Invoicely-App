fe14ed20c44131183cd3bef483d74097151e6a98
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-08-26 14:58:06 +0530

    client feature

---
+++ lib/features/client/presentation/widgets/client_info_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClientInformationCard extends StatelessWidget {
+  const ClientInformationCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/features/client/presentation/widgets/client_information_card.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class ClientInforCard extends StatelessWidget {
+  const ClientInforCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
