a9a4b5096d72dc448e4f4ea97a1c2c12a90aba53
iamayushsharmaa <ayushsharmagit@gmail.com>
2026-05-01 03:42:47 +0530

    ref: removed dependencies from main class and used di and clean classs

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,35 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:invoice/config/routes.dart';
+import 'package:invoice/core/di/injections.dart';
 import 'package:invoice/core/theme/app_theme.dart';
-import 'package:invoice/features/auth/data/remote/auth_remote_datasource_impl.dart';
-import 'package:invoice/features/auth/data/repository/auth_repository_impl.dart';
 import 'package:invoice/features/auth/presentation/bloc/auth_bloc.dart';
 
-import 'features/auth/domain/repository/auth_repository.dart';
 import 'features/invoice/data/model/invoice_response.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final dio = Dio();
-  final googleSignIn = GoogleSignIn();
-  final storage = FlutterSecureStorage();
-
-  final authService = AuthService(
-    dio: dio,
-    googleSignIn: googleSignIn,
-    storage: storage,
-  );
-  final authRepository = AuthRepositoryImpl(authService);
+  await dotenv.load(fileName: '.env');
 
   await Hive.initFlutter();
   Hive.registerAdapter(InvoiceResponseAdapter());
 
   await Hive.openBox<InvoiceResponse>('invoiceCache');
-  runApp(MyApp(authRepository));
+  await initDependencies();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final AuthRepository authRepository;
-
-  const MyApp(this.authRepository, {super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<AuthBloc>(
-          create: (context) =>
-              AuthBloc(repository: authRepository)..add(CheckAuthStatus()),
-        ),
-      ],
+    return BlocProvider<AuthBloc>(
+      create: (_) => sl<AuthBloc>()..add(CheckAuthStatusEvent()),
       child: Builder(
         builder: (context) {
           return MaterialApp.router(
